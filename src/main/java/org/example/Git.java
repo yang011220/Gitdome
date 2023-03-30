d2ab09db6a1358dd6eae6773bc16faa55a353872
011220ly <156921200@qq.com>
2023-03-30 14:24:55 +0800

    sects commit

--- src/main/java/org/example/Git.java
+++ src/main/java/org/example/Git.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Git {
     public static void main(String[] args) {
-        System.out.println("Git");
-        System.out.println("Git");
+        System.out.println("Git1");
+        System.out.println("Git2");
+        System.out.println("Git3");
     }
 }
